c6f766b59be99480389fc95ae5ee9621a315b713
Bin Wang <binwang.0213@gmail.com>
2019-07-09 11:21:10 -0500

    Update FracsGeo.py

--- PyDFN3D/DFN_Geometry/FracsGeo.py
+++ PyDFN3D/DFN_Geometry/FracsGeo.py
@@ -43,8 +43,8 @@ class FractureNetworks:
         IntersectsLines-- [Intersect Id]Pts ID Table for each intersection, each intersectin line have two ids
         IntersectFracs -- [Intersect Id]Fracture ID Table for each intersection
         FracIntersects -- [Frac Id] Intersections id for each fracture
-        WellFracIntersects -- [Intersect Id] Well id and node id for each well intersection
-        IntersectWellFracs -- [Frac Id] Intersect IDs for each well 
+        WellFracIntersects -- [Well Id] Frac id and node id for each well 
+        FracsIntersectWell -- [Frac Id] Well id and node id for each fracture
         ClusterFracs   -- [Cluster ID] Fractures id for each cluster
 
         Author:Bin Wang(binwang.0213@gmail.com)
@@ -68,7 +68,7 @@ class FractureNetworks:
         self.IntersectFracs = []
         self.FracIntersects = []
         self.WellFracIntersects = []
-        self.IntersectWellFracs = []
+        self.FracsIntersectWell = []
         
         self.ClusterFracs = []
     
@@ -136,7 +136,7 @@ class FractureNetworks:
         
         #Collect Well node info
         Well_node_3D= []
-        NumWells=len(self.WellFracIntersects)
+        NumWells=len(self.IntersectWellFracs)
         
         return Boundary_vert_3D, Intersect_vert_3D
 
